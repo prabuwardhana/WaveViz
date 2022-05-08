474fef361d9468c9547c8834ec60ba82cbee2665
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2022-05-08 19:00:25 +0900

    add OpenFileButton component

---
+++ src/components/Buttons/OpenFileButton.jsx
@@ -0,0 +1,35 @@
+import { FolderOpen } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import React, { useContext } from "react";
+import { FileContentContext } from "../../store/stores";
+
+const OpenFileButton = () => {
+  const [, dispatch] = useContext(FileContentContext);
+
+  const handleClick = async () => {
+    // Calling loadFile() will invoke the load-file event and return a promise.
+    // Once main process sent a respond, the promise is resolved.
+    const content = await window.electronApi.loadFile();
+
+    // As soon as we have received a respond from main process,
+    // store the respond (file content) into global state.
+    // The file content will be used by the other components.
+    content &&
+      dispatch({
+        type: "SET_CONTENT",
+        payload: content,
+      });
+  };
+
+  return (
+    <Button
+      variant="contained"
+      startIcon={<FolderOpen />}
+      onClick={handleClick}
+    >
+      Open CSV
+    </Button>
+  );
+};
+
+export default OpenFileButton;
